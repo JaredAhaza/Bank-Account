553b8b577654e9c2971b098ab3dc95884982c7b8
JaredAhaza <jaredahazq@gmail.com>
2018-04-06 15:04:56 +0300

    updated page

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,49 @@
-//User interface
-$(document).ready(function() {
-  $("form#Enter").submit(function(event) {
-    event.preventDefault();
-var name = $('#name').val();
-var initialdeposit = $('#initialDeposit').val();
-var depositamount = $('#deposit').val();
-var withdrawamount = $('#withdraw').val();
-//backend logic
-function Bank(name, initialDeposit, withdrawamount, depositamount) {
-this.name = name;
-this.withdrawamount = initialDeposit;
-this.depositamount = AmountW;
-this.depositamount = AmoutD
-};
-
-Bank.prototype.getName = function() {
-  return this.name
-};
-Bank.prototype.getinitialDeposit = function() {
-  return this.initialDeposit
-};
-Bank.prototype.getwithdrawamount = function() {
-  return this.withdrawamount
-};
-Bank.prototype.getdepositamount = function() {
-  return this.depositamount
-};
-function currentbalance(balance){
-
+function AccountDetails (name, initial){
+    this.names = name;
+    this.initialDeposit = initial;
+    this.newBal = initial;
 }
+AccountDetails.prototype.depositAmount = function(deposit){
+    this.newBal = this.newBal+deposit;
+    return this.newBal;
+}
+AccountDetails.prototype.withdrawAmount = function(withdraw){
+    this.newBal = this.newBal-withdraw;
+    return this.newBal;
+}
+$(document).ready(function(){
+    var accounts = [];
+    $("form#Account").submit(function(event){
+        event.preventDefault();
+        var inputtedNames = $("input#name").val();
+        var inputtedDeposit = parseInt($("input#initialDeposit").val());
+        var newAccount = new AccountDetails(inputtedNames, inputtedDeposit);
+        accounts.push(newAccount);
+
+        $("ol#accounts").append("<li><span class=account>"+newAccount.names+"</span></li>");
+
+        $(".account").last().click(function(){
+            var index = accounts.indexOf(newAccount);
+            $("#showAccount h2").text(newAccount.names);
+            $(".names").text(newAccount.names);
+            $(".initial").text(newAccount.initialDeposit);
+            $(".balance").text(newAccount.newBal);
+            $("form#amount-change").data("id",index);
+        });
+    });
+    $("button#deposit").click(function (event) {
+        event.preventDefault();
+        var index = $("form#amount-change").data("id");
+        var findAccount = accounts[index];
+        var inputtedNewAmount = parseInt($("input#initialDeposit").val());
+        $(".balance").html(findAccount.depositAmount(inputtedNewAmount));
+    });
+    $("button#withdraw").click(function (event) {
+        event.preventDefault();
+        var index = $("form#amount-change").data("id");
+        var findAccount = accounts[index];
+        var inputtedNewAmount = parseInt($("input#withdrawal").val());
+        $(".balance").html(findAccount.withdrawAmount(inputtedNewAmount));
+    });
+
+});
